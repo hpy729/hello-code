401ff4aeae1156eee8c804d16083b500789e07f5
haianhuang <hpy729@gmail.com>
2020-02-10 15:03:31 +0800

    new file:   calculator.exe 	modified:   haha.py 	new file:   hello.py 	deleted:    jisuanqi.py 	new file:   today.py

--- haha.py
+++ haha.py
@@ -1,8 +1,8 @@
-# list1=range(1,101)
-# for i in list1:
-#     a=str(i).find('7')
-#     if int(i)%7 ==0 or a != -1:
-#         print(i)
+list1=range(1,101)
+for i in list1:
+    a=str(i).find('7')
+    if int(i)%7 ==0 or a != -1:
+        print(i)
 
 import math as m
 
---
+++ today.py
@@ -0,0 +1,2 @@
+from datetime import datetime
+print(datetime.now().strftime('今天是%y年%m月%d日'))
